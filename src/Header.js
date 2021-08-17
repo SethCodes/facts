8c68854d6e80b60adc132ede7a8abee5504428a6
Seth Stephens <sethastephens8@gmail.com>
2021-08-17 09:56:54 -0400

    push

--- src/Header.js
+++ src/Header.js
@@ -1,38 +1,52 @@
-import React from 'react';
-import Facts from './facts';
-import Wave from '../src/wave.svg';
-import {BrowserRouter as Router, Link } from 'react-router-dom';
-
-
-
+import React from "react";
+import Facts from "./facts";
+import Wave from "../src/wave.svg";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 const Header = () => {
+  const handleClick = (e) => {
+    //e.preventDefault();
+  };
 
-const handleClick = (e) => {
-        //e.preventDefault();
-    }
-
-
-
-    return (
-        <div id="header">
-        <div className="headDiv">
+  return (
+    <div id="header">
+      <div className="headDiv">
         <div className="headCont text-center">
-        <Link to={'/facts/'}>
-            <h1 className="">header component</h1></Link>
-            <p className="text-center">Would you like to know some fun facts about Web Developers? Click an option below to learn a few things.</p>
+          <Link to={"/facts/"}>
+            <h1 className="">Fun Facts About Web Developers</h1>
+          </Link>
+          <p className="text-center">
+            Would you like to know some fun facts about Web Developers? Click an
+            option below to learn a few things.
+          </p>
         </div>
-           <div className="" id="buttons">
-           <Link to={'/facts/' + Facts[0].id} className="btn btn-outline-dark" onClick={handleClick}>Fact 1</Link>
-           <Link to={'/facts/' + Facts[1].id} className="btn btn-outline-dark" onClick={handleClick}>Fact 2</Link>
-           <Link to={'/facts/' + Facts[2].id} className="btn btn-outline-dark" onClick={handleClick}>Fact 3</Link>
-               {/* <a href={'/' + Facts[0].id} className="btn btn-outline-dark" onClick={handleClick}>Fact 1</a>
-               <a href={'/' + Facts[1].id} className="btn btn-outline-dark" onClick={handleClick}>Fact 2</a>
-               <a href={'/' + Facts[2].id} className="btn btn-outline-dark" onClick={handleClick}>Fact 3</a> */}
-           </div></div>
-           <img src={Wave} alt=""/>
+        <div className="" id="buttons">
+          <Link
+            to={"/facts/" + Facts[0].id}
+            className="btn btn-outline-dark"
+            onClick={handleClick}
+          >
+            Fact 1
+          </Link>
+          <Link
+            to={"/facts/" + Facts[1].id}
+            className="btn btn-outline-dark"
+            onClick={handleClick}
+          >
+            Fact 2
+          </Link>
+          <Link
+            to={"/facts/" + Facts[2].id}
+            className="btn btn-outline-dark"
+            onClick={handleClick}
+          >
+            Fact 3
+          </Link>
         </div>
-    )
-}
+      </div>
+      <img src={Wave} alt="" />
+    </div>
+  );
+};
 
-export default Header
+export default Header;
